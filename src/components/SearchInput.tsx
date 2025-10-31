2c1b676f679d7a9819ffec17bc675425326d8ca6
Olena457 <olenademianenko42@gmail.com>
2025-10-31 15:38:27 +0200

    styles input

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -10,17 +10,17 @@ export default function SearchInput(props: SearchBoxProps) {
   return (
     <form
       onSubmit={props.onSubmit}
-      className={`flex items-center  bg-white rounded-md  px-3 py-2  max-w-md w-full  ${props.className}`}
+      className={`flex items-center glass-input-card  rounded-md  px-3 py-2  max-w-md w-full  ${props.className}`}
     >
       <input
         type="text"
         value={props.value}
         onChange={props.onChange}
         placeholder="Search location.."
-        className="px-4 py-2 w-[230px] border border-gray-300 rounded-l-md focus:outline-none  focus:border-gray-500 h-full"
+        className="px-4 py-2 w-[230px] border border-blue-100-300 rounded-l-md focus:outline-none  focus:border-blue-300 h-full"
       />
-          <button className="px-4 py-[9px] bg-gray-500 text-white rounded-r-md focus:outline-none hover:bg-gray-400  h-full">
-              <Search/>
+      <button className="px-4 py-[9px] bg-blue-300 text-blue-100 rounded-r-md focus:outline-none hover:bg-blue-200  h-full">
+        <Search className="text-gray-500"/>
       </button>
     </form>
   );
--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,16 +1,4 @@
-// import { mergeClasses } from "../utils/merge";
 
-// export default function Wrapper(props: React.HTMLProps<HTMLDivElement>) {
-//     return (
-//       <div
-//         {...props}
-//         className={mergeClasses(
-//           "w-full bg-blue-200 border rounded-4xl flex py-4 px-2 sm:px-4  sm:py-8 shadow-sm",
-//           props.className
-//         )}
-//       />
-//     );
-// }
 import { mergeClasses } from "../utils/merge";
 
 export default function Wrapper(props: React.HTMLProps<HTMLDivElement>) {
