bca532f654d9a0ed8ea9fa5b9a0ed3f6861e887a
Olena457 <olenademianenko42@gmail.com>
2025-11-02 10:07:43 +0200

    fixed styles

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -3,7 +3,7 @@ export default function Skeleton() {
     <div className="flex flex-col gap-9 w-full pb-10 pt-4 animate-pulse">
       <section className="space-y-4">
         <div className="h-8 w-48 bg-gray-300 rounded-md"></div>
-        <div className="w-full bg-gray-200 border border-gray-300 rounded-[40px] shadow-md h-[350px] sm:h-[450px]">
+        <div className="w-full bg-gray-300 border border-gray-300 rounded-[40px] shadow-md h-[350px] sm:h-[450px]">
           <div className="h-full w-full bg-gray-300/50 rounded-[40px]"></div>
         </div>
       </section>
@@ -12,7 +12,7 @@ export default function Skeleton() {
         {[1, 2].map((index) => (
           <div
             key={index}
-            className="w-full h-16 bg-gray-200 border border-gray-300 rounded-[40px] shadow-sm"
+            className="w-full h-16 bg-gray-300 border border-gray-400 rounded-[40px] shadow-sm"
           ></div>
         ))}
       </section>
